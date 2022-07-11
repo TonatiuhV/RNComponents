de0c1770c55fd928808cdda652dbe8d11f9f012b
= <=>
2022-07-10 21:45:32 -0500

    Flatlist

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import {View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
+interface MenuItem {
+  name: string;
+  icon: string;
+  component: string;
+}
+
+const menuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+];
+
 export const HomeScreen = () => {
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text>{menuItem.name}</Text>
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Icon name="star-outline" size={50} color="black" />
+    <View style={{flex: 1}}>
+      <FlatList
+        data={menuItems}
+        // Recibé como objecto las propiedades  item y index
+        renderItem={({item}) => renderMenuItem(item)}
+        // Simpre poner el key estractor retornando algo unico de tipo string
+        keyExtractor={item => item.name}
+      />
     </View>
   );
 };
