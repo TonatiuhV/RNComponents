ec5a0c121253a9e90542c2295ee5b0138c37984b
= <=>
2022-07-10 21:31:43 -0500

    Intalacion Stack and Icons

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
-import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import Navigator from './src/navigator/Navigator';
 
 const App = () => {
   return (
-    <View>
-      <Text>App Components</Text>
-    </View>
+    <NavigationContainer>
+      <Navigator />
+    </NavigationContainer>
   );
 };
 
