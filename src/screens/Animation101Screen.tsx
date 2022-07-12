f3473af8de325835d3db058c1b08df1282c3ee79
= <=>
2022-07-11 19:53:50 -0500

    FadeIn y FadeOut

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,13 +1,34 @@
 import React, {useRef} from 'react';
-import {Animated, StyleSheet, View} from 'react-native';
+import {Animated, Button, StyleSheet, View} from 'react-native';
 
 export const Animation101Screen = () => {
   //no renderia el componente
-  const opacity = useRef(new Animated.Value(0.4)).current;
+  const opacity = useRef(new Animated.Value(0)).current;
+
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() =>
+      // Puede resbir un callback al finalizar la animacion
+      console.log('La animacion termino'),
+    ); //  para que la animacion empieze
+  };
+
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(); //  para que la animacion empieze
+  };
 
   return (
     <View style={styles.container}>
       <Animated.View style={[styles.purpleBox, {opacity: opacity}]} />
+      <Button title="FadeIn" onPress={fadeIn} />
+      <Button title="FadeOut" onPress={fadeOut} />
     </View>
   );
 };
@@ -20,6 +41,7 @@ const styles = StyleSheet.create({
   },
   purpleBox: {
     backgroundColor: '#5856d6',
+    marginBottom: 20,
     width: 150,
     height: 150,
   },
