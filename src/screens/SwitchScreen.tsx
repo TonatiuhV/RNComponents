a8b3642a977242877bb523672db41360cba69706
= <=>
2022-07-11 21:32:05 -0500

    creacion de switch

---
+++ src/screens/SwitchScreen.tsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import {Platform, Switch, View} from 'react-native';
+
+export const SwitchScreen = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(!isEnabled);
+
+  return (
+    <View>
+      <Switch
+        trackColor={{false: '#D9D9DB', true: '#5856D6'}}
+        thumbColor={Platform.OS === 'android' ? '#5856D6' : ''}
+        // ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+    </View>
+  );
+};
