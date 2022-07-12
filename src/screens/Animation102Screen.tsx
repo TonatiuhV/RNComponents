86bd8e391d1666b263a6448179a4de3957acf1f4
= <=>
2022-07-11 21:04:29 -0500

    Animeted Value XY

--- src/screens/Animation102Screen.tsx
+++ src/screens/Animation102Screen.tsx
@@ -1,17 +1,50 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useRef} from 'react';
+import {Animated, PanResponder, StyleSheet, View} from 'react-native';
 
 export const Animation102Screen = () => {
+  // pan animmated
+  const pan = useRef(new Animated.ValueXY()).current;
+  // panresponsender
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: Animated.event(
+      [
+        null,
+        {
+          dx: pan.x, // x,y are Animated.Value
+          dy: pan.y,
+        },
+      ],
+      {
+        useNativeDriver: false, // Es necesario pasarlo para no generar un warining
+      },
+    ),
+    onPanResponderRelease: () => {
+      Animated.spring(
+        pan, // Auto-multiplexed ///// el driver debe estar el false
+        {toValue: {x: 0, y: 0}, useNativeDriver: false}, // Back to zero
+      ).start();
+    },
+  });
   return (
-    <View style={{flex: 1}}>
-      <View style={styles.purpleBox}></View>
+    <View style={styles.container}>
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[pan.getLayout(), styles.purpleBox]}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   purpleBox: {
-    backgroundColor: '#5856d6',
+    backgroundColor: '#75CEDB',
     width: 150,
     height: 150,
   },
