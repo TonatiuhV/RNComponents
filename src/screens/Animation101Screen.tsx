1388de43a4c057b655e3113aec66304c99300ce5
= <=>
2022-07-11 20:25:13 -0500

    Easing Bounce Animation

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,9 +1,10 @@
 import React, {useRef} from 'react';
-import {Animated, Button, StyleSheet, View} from 'react-native';
+import {Animated, Button, Easing, StyleSheet, View} from 'react-native';
 
 export const Animation101Screen = () => {
   //no renderia el componente
   const opacity = useRef(new Animated.Value(0)).current;
+  const top = useRef(new Animated.Value(-100)).current;
 
   const fadeIn = () => {
     Animated.timing(opacity, {
@@ -14,19 +15,44 @@ export const Animation101Screen = () => {
       // Puede resbir un callback al finalizar la animacion
       console.log('La animacion termino'),
     ); //  para que la animacion empieze
+    Animated.timing(top, {
+      toValue: 0,
+      duration: 800,
+      useNativeDriver: true,
+      easing: Easing.bounce, // Nos prove de varias animaciones
+    }).start();
   };
 
   const fadeOut = () => {
     Animated.timing(opacity, {
       toValue: 0,
-      duration: 300,
+      duration: 800,
       useNativeDriver: true,
     }).start(); //  para que la animacion empieze
+    //top.setValue(-100); // Resetemos el valor
+    Animated.timing(top, {
+      toValue: -100,
+      duration: 600,
+      useNativeDriver: true,
+      easing: Easing.out(Easing.exp), // Nos prove de varias animaciones
+    }).start();
   };
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.purpleBox, {opacity: opacity}]} />
+      <Animated.View
+        style={[
+          styles.purpleBox,
+          {
+            opacity: opacity,
+            transform: [
+              {
+                translateY: top,
+              },
+            ],
+          },
+        ]}
+      />
       <Button title="FadeIn" onPress={fadeIn} />
       <Button title="FadeOut" onPress={fadeOut} />
     </View>
