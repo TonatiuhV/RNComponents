ca3938342c341255f2c89a1c7005775a02016643
= <=>
2022-07-12 19:59:27 -0500

    CustomeSwitches

--- src/screens/SwitchScreen.tsx
+++ src/screens/SwitchScreen.tsx
@@ -1,21 +1,62 @@
 import React, {useState} from 'react';
-import {Platform, Switch, View} from 'react-native';
+import {Platform, StyleSheet, Switch, Text, View} from 'react-native';
+import {CustomeSwitch} from '../components/CustomeSwitch';
 import {HeaderTitle} from '../components/HeaderTitle';
 
 export const SwitchScreen = () => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(!isEnabled);
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+
+  const {isActive, isHungry, isHappy} = state;
+  const onChange = (value: boolean, field: string) => {
+    setState({
+      ...state,
+      [field]: value,
+    });
+  };
 
   return (
     <View style={{marginHorizontal: 20}}>
       <HeaderTitle title="Switches" />
-      <Switch
-        trackColor={{false: '#D9D9DB', true: '#5856D6'}}
-        thumbColor={Platform.OS === 'android' ? '#5856D6' : ''}
-        // ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isActive</Text>
+        <CustomeSwitch
+          onChange={isEnabled => onChange(isEnabled, 'isActive')}
+          isOn={isActive}
+        />
+      </View>
+      {/* isHungry */}
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isHungry</Text>
+        <CustomeSwitch
+          onChange={isEnabled => onChange(isEnabled, 'isHungry')}
+          isOn={isHungry}
+        />
+      </View>
+      {/* isHappy */}
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isHappy</Text>
+        <CustomeSwitch
+          onChange={isEnabled => onChange(isEnabled, 'isHappy')}
+          isOn={isHappy}
+        />
+      </View>
+      <Text style={styles.switchText}>{JSON.stringify(state, null, 5)}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  switchRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  switchText: {
+    fontSize: 25,
+  },
+});
